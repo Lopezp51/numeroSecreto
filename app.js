723e918cd0b0bf096d7abe5b0406191e0818549c
Pedro <lopesp5151@gmail.com>
2023-11-08 22:21:46 -0300

    alterando limite para 100

---
+++ app.js
@@ -0,0 +1,35 @@
+alert('Bem vindo ao jogo do número secreto!');
+let numeroMax = 100;
+const numeroSecreto = parseInt(Math.random() * numeroMax + 1)
+console.log(numeroSecreto)
+let chute 
+let tentativa = 1
+
+
+while(numeroSecreto != chute) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMax}`)
+
+    if(numeroSecreto == chute) {
+        break;
+        alert('Você descobriu o número secreto ' + numeroSecreto + '!');
+        alert(`Levou ${tentativa} tentativas!`)
+    } else {
+        
+        if (numeroSecreto > chute) {
+            alert(`O número é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`)
+        }
+        tentativa ++
+    } 
+}
+
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa'
+
+alert(`Vc descobriu o número secreto ${numeroSecreto}, com ${tentativa} ${palavraTentativa}!`)
+
+// if( tentativa > 1) {
+//     alert(`Vc descobriu o número secreto ${numeroSecreto}, com ${tentativa} tentativas!`)
+// } else {
+//     alert(`Vc descobriu o número secreto ${numeroSecreto}, com ${tentativa} tentativa!`)
+// }
